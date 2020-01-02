c73e1c2084e287827059dcbf6ff2526d6e6257a4
gttdo <vinces.gerardo@gmail.com>
2020-01-01 17:36:31 -0800

    add classes, album, artist and song

---
+++ album.php
@@ -0,0 +1,28 @@
+<?php include("includes/header.php");
+
+if(isset($_GET['id'])){
+  $albumId = $_GET['id'];
+}
+else {
+  header("location: index.php");
+}
+
+$album = new Album($con, $albumId);
+$artist = $album->getArtist();
+?>
+
+<div class="entityInfo">
+
+  <div class="leftSection">
+    <img src="<?php echo $album->getArtworkPath(); ?>" alt="album artwork">
+  </div>
+  <div class="rightSection">
+    <h2><?php echo $album->getTitle(); ?></h2>
+    <p>By <?php echo $artist->getName(); ?></p>
+    <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+
+  </div>
+</div>
+
+
+<?php include("includes/footer.php"); ?>

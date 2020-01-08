a25b3eaeb9dfa1196be34c27edd489f5fa2674aa
gttdo <vinces.gerardo@gmail.com>
2020-01-07 20:26:00 -0800

    updated header and nowplayingbar files

--- includes/header.php
+++ includes/header.php
@@ -18,13 +18,15 @@ if(isset($_SESSION['userLoggedIn'])){
  ?>
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
   <head>
-    <meta charset="utf-8">
     <title>Welcome to Puppify</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
   </head>
   <body>
+
     <div id="mainContainer">
       <div id="topContainer">
         <?php include("includes/navBarContainer.php"); ?>

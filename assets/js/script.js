97ce21c7df0a8178a95715549de05807e499843c
gttdo <vinces.gerardo@gmail.com>
2020-01-12 21:17:19 -0800

    added features in progress bar and volume bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,59 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
+
+function formatTime(seconds){
+  var time = Math.round(seconds);
+  var minutes = Math.floor(time / 60); //Rounds down
+  var seconds = time - minutes * 60;
+
+  //Conditional version 1
+  // var extraZero;
+  //
+  // if(seconds < 10){
+  //   extraZero = "0";
+  // } else {
+  //   extraZero = "";
+  // }
+
+  //Conditional version 2
+  var extraZero = (seconds < 10) ? "0" : "";
+  return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio){
+  $(".progressTime.current").text(formatTime(audio.currentTime));
+  $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+  var progress = audio.currentTime / audio.duration * 100;
+  $(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio){
+  var volume = audio.volume  * 100;
+  $(".volumebar .progress").css("width", volume + "%");
+}
 
 function Audio(){
 
   this.currentlyPlaying;
   this.audio = document.createElement('audio');
+
+  this.audio.addEventListener("canplay", function(){
+    //this refers to the object that the event was called on
+    var duration = formatTime(this.duration);
+    $(".progressTime.remaining").text(duration);
+  });
+
+  this.audio.addEventListener("timeupdate", function(){
+    if(this.duration){
+      updateTimeProgressBar(this)
+    }
+  });
+
+  this.audio.addEventListener("volumechange", function(){
+    updateVolumeProgressBar(this);
+  });
 
   this.setTrack = function(track){
     this.currentlyPlaying = track;
@@ -16,5 +65,9 @@ function Audio(){
   }
   this.pause = function(){
     this.audio.pause();
+  }
+
+  this.setTime = function(seconds){
+    this.audio.currentTime = seconds;
   }
 }
--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -17,7 +17,55 @@ $jsonArray = json_encode($resultArray);
     currentPlaylist = <?php echo $jsonArray; ?>;
     audioElement = new Audio();
     setTrack(currentPlaylist[0], currentPlaylist, false);
+    updateVolumeProgressBar(audioElement.audio);
+
+    $("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e){
+      e.preventDefault();
+    });
+
+    //Playback Bar Progress
+    $(".playbackBar .progressBar").mousedown(function(){
+      mouseDown = true;
+    });
+    $(".playbackBar .progressBar").mousemove(function(e){
+      if(mouseDown == true){
+        //set time of song, depending on position of mouse
+        timeFromOffset(e, this);
+      }
+    });
+    $(".playbackBar .progressBar").mouseup(function(e){
+      timeFromOffset(e, this);
+    });
+
+    //Volume Bar Progress
+    $(".volumebar .progressBar").mousedown(function(){
+      mouseDown = true;
+    });
+    $(".volumebar .progressBar").mousemove(function(e){
+      if(mouseDown == true){
+        var percentage = e.offsetX / $(this).width();
+        if(percentage >= 0 && percentage <= 1){
+          audioElement.audio.volume = percentage;
+        }
+      }
+    });
+    $(".volumebar .progressBar").mouseup(function(e){
+      var percentage = e.offsetX / $(this).width();
+      if(percentage >= 0 && percentage <= 1){
+        audioElement.audio.volume = percentage;
+      }
+    });
+
+    $(document).mouseup(function(){
+      mouseDown = false;
+    });
   });
+
+  function timeFromOffset(mouse, progressBar){
+    var percentage = mouse.offsetX / $(progressBar).width() * 100;
+    var seconds = audioElement.audio.duration * (percentage / 100);
+    audioElement.setTime(seconds);
+  }
 
   function setTrack(trackId, newPlaylist, play){
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId}, function(data){

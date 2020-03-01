818572fc62618daad316c6af796478f601e5aa75
gttdo <vinces.gerardo@gmail.com>
2020-03-01 09:15:15 -0800

    puppify update

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+include("includes/includedFiles.php");
+ ?>
+
+ <div class="playListsContainer">
+   <div class="griedViewContainer">
+     <h2>PLAYLISTS</h2>
+     <div class="buttonItems">
+       <button class="button green">NEW PLAYLIST</button>
+     </div>
+   </div>
+ </div>
